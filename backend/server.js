73d4683e88eb4a0ad86d764a83463ca2af79fd00
Milan Sony <milansonyofficial@gmail.com>
2025-01-16 10:28:21 +0530

    feat: server created

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,10 @@ import dotenv from "dotenv"
 
 // config dot env
 dotenv.config()
+
+// creates an app express app
+const app = express()
+
+app.listen((process.env.PORT || 5000), () => {
+    console.log(`\n🚀 Server listening on port: ${process.env.PORT || 5000}`)
+})
