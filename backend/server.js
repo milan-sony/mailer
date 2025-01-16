735a79ff0775576869ea31326bffc9ec1b9b4e29
Milan Sony <milansonyofficial@gmail.com>
2025-01-16 11:46:42 +0530

    feat: db configured

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import routes from "./routes/index.js"
+import connectDB from "./configs/db.js"
 
 // config dot env
 dotenv.config()
 
 // creates an app express app
 const app = express()
+
+// connect with DB
+connectDB()
 
 // base URL
 app.use("/", routes)
