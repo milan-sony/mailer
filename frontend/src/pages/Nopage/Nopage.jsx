e72e98755bc41e46b02b2964fa69cb6942d53866
Milan Sony <milansonyofficial@gmail.com>
2025-01-21 09:54:37 +0530

    style: page not found created

---
+++ frontend/src/pages/Nopage/Nopage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nopage() {
+    return (
+        <div className='flex flex-col justify-center items-center h-dvh'>
+            <h1 className='text-5xl font-black'>404</h1>
+            <h3 className='text-2xl font-semibold font-mono'>Page Not Found !</h3>
+        </div>
+    )
+}
+
+export default Nopage
