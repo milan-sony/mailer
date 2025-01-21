d367fe4447b2efe4ff21ae84688cb69ef4c2908b
Milan Sony <milansonyofficial@gmail.com>
2025-01-21 09:55:09 +0530

    style: index page created

--- frontend/src/pages/Indexpage/Indexpage.jsx
+++ frontend/src/pages/Indexpage/Indexpage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
+
 
 function Indexpage() {
     return (
-        <div>
-            <h1 className='text-6xl'>Index page</h1>
+        <div className='h-dvh flex justify-center items-center'>
+            <Link to={"/homepage"} className='bg-black w-32 h-10 text-white font-sans font-medium rounded-md capitalize flex justify-center items-center hover:bg-slate-500'>Compose Mail</Link>
         </div>
     )
 }
