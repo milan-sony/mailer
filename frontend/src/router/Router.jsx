a0caf749b76bd9aef0f9dc4fa5afe19b43eb1ed9
Milan Sony <milansonyofficial@gmail.com>
2025-01-21 17:22:52 +0530

    feat: router redirect added

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Indexpage from '../pages/Indexpage/Indexpage';
 import Homepage from '../pages/Homepage/Homepage';
 import Nopage from '../pages/Nopage/Nopage';
+import { mailControllerStore } from '../store/mailControllerStore';
+import Sendmailpage from '../pages/Sendmailpage/Sendmailpage';
 
 function Router() {
+
+    const { isMailComposed, isMailSendSuccessfully } = mailControllerStore()
+
     return (
         <Routes>
             <Route path="/" element={<Indexpage />} />
-            <Route path='/homepage' element={<Homepage />} />
+            <Route path='/homepage' element={!isMailComposed ? <Homepage /> : <Navigate to={"/sendmail"} />} />
+            <Route path='/sendmail' element={isMailSendSuccessfully ? <Sendmailpage /> : <Navigate to={"/homepage"} />} />
             <Route path="*" element={<Nopage />} />
         </Routes>
     )
