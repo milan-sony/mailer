16c090233168f031d8e35bc97e82a2a69c0592fb
Milan Sony <milansonyofficial@gmail.com>
2025-01-21 15:22:36 +0530

    chore: mail items added to state via enter key press

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Upload } from 'lucide-react'
 
 function Homepage() {
+    const [mails, setMails] = useState([])
+
+    // gets mail id's
+    const handleChange = (e) => {
+        if (e.key !== 'Enter') return
+        const value = e.target.value
+        if (!value.trim()) return
+        setMails([...mails, value])
+        e.target.value = ""
+    }
+
+    // remove mail id's
+    const removeMail = (index) => {
+        setMails(mails.filter((el, i) => i !== index))
+    }
+
+    // form submit
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="w-full h-screen">
             {/* mail form */}
@@ -12,30 +33,26 @@ function Homepage() {
                             <h2 className="text-2xl uppercase font-black mb-1 font-Open-Sans text-black">Mailer</h2>
                             <p className="text-gray-600 mb-6 text-sm font-Open-Sans">Compose your mail !</p>
                             <p className="text-black mb-4 text-sm font-Open-Sans"><b>From: name@domain.com</b></p>
-                            <form autoComplete="off" noValidate>
+                            <form autoComplete="off" noValidate onSubmit={handleSubmit}>
                                 <div className="space-y-2 mb-2">
                                     <label className="text-black mb-2 block">To: (Enter the mail id's)</label>
                                     <div className='bg-gray-100 max-h-40 overflow-y-auto rounded-md p-2'>
                                         <div className='flex items-center flex-wrap gap-4'>
-                                            <div className='bg-white inline-block rounded-md p-1'>
-                                                <span className='mr-2'>milan@gmail.com</span>
-                                                <span className='bg-red-500 size-5 text-white rounded-full inline-flex justify-center items-center text-center cursor-pointer text-lg font-semibold'>&times;</span>
-                                            </div>
-                                            <div className='bg-white inline-block rounded-md p-1'>
-                                                <span className='mr-2'>milan@gmail.com</span>
-                                                <span className='bg-red-500 size-5 text-white rounded-full inline-flex justify-center items-center text-center cursor-pointer text-lg font-semibold'>&times;</span>
-                                            </div>
-                                            <div className='bg-white inline-block rounded-md p-1'>
-                                                <span className='mr-2'>milan@gmail.com</span>
-                                                <span className='bg-red-500 size-5 text-white rounded-full inline-flex justify-center items-center text-center cursor-pointer text-lg font-semibold'>&times;</span>
-                                            </div>
-                                            <input type="text" placeholder="Enter mail id's" className='outline-none border-none rounded-md bg-white p-1 text-black' />
+                                            {
+                                                mails.map((mailId, index) => (
+                                                    <div className='bg-white inline-block rounded-md p-1' key={index}>
+                                                        <span className='mr-2'>{mailId}</span>
+                                                        <span className='bg-red-500 size-5 text-white rounded-full inline-flex justify-center items-center text-center cursor-pointer text-lg font-semibold' onClick={() => removeMail(index)}>&times;</span>
+                                                    </div>
+                                                ))
+                                            }
+                                            <input type="text" placeholder="Enter mail id's" className='outline-none border-none rounded-md bg-white p-1 text-black' onKeyDown={handleChange} />
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="mt-6 mb-2 w-full flex justify-end">
-                                    <button type='submit' className='p-2 text-center flex justify-between text-white text-sm bg-gray-600 rounded hover:text-white hover:bg-black capitalize font-medium'><Upload className='size-5 mr-1'/>upload file
+                                    <button type='submit' className='p-2 text-center flex justify-between text-white text-sm bg-gray-600 rounded hover:text-white hover:bg-black capitalize font-medium'><Upload className='size-5 mr-1' />upload file
                                     </button>
                                 </div>
 
@@ -52,7 +69,7 @@ function Homepage() {
                                     </div>
                                 </div>
                                 <div className="mt-6">
-                                    <button type='submit' className='block w-full py-2 text-center text-white bg-gray-600 rounded hover:text-white hover:bg-black uppercase font-medium'>Send mail
+                                    <button type='submit' className='block w-full py-2 text-center text-white bg-gray-600 rounded hover:text-white hover:bg-black uppercase font-medium'>Compose mail
                                     </button>
                                 </div>
                             </form>
